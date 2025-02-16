c9d8e3ee181dcf476df78e1e58e04bcaa43666fb
吾即萌新 <169010883+wujimengxin@users.noreply.github.com>
2025-02-16 12:34:05 +0800

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-    let border = document.querySelector('#animation_border').querySelectorAll('li');
-
-    for (var i = 0; i < border; i++) {
-        console.log('1111');
-    }
-})
