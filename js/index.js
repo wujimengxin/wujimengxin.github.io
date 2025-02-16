6096a1b3de8ddb136ccf752e49978067b995c2ff
吾即萌新 <169010883+wujimengxin@users.noreply.github.com>
2025-02-16 12:31:15 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    let border = document.querySelector('#animation_border').querySelectorAll('li');
+
+    for (var i = 0; i < border; i++) {
+        console.log('1111');
+    }
+})
